0f231e8322b78db7ab021eafe8e3fd42704683fa
soapy-bat <157433485+soapy-bat@users.noreply.github.com>
2024-03-04 00:48:55 +0100

    major update
    - general cleanup and layout
    - added header

--- soapy-seahorse_Fades_AuditionXOut.lua
+++ soapy-seahorse_Fades_AuditionXOut.lua
@@ -1,10 +1,39 @@
--- user settings
+--[[
+
+source-destination fades: audition right side, mute left side
+
+This script is part of the soapy-seahorse package.
+It requires the file "soapy-seahorse_Fades_Functions.lua"
+
+(C) 2024 the soapy zoo
+copyleft: chmaha
+thanks: fricia, X-Raym, GPT3.5
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <https://www.gnu.org/licenses/>.
+]]
+
+-------------------
+-- user settings --
+-------------------
+
 local preRoll = 0                   -- audition pre-roll, in seconds
 local postRoll = 2                  -- audition post-roll, in seconds
-local fadeLenMultiplier = 0         -- 0, ..., 1 /// 0.5: center of fade
+local cursorBias = 0                -- 0, ..., 2 /// 1: center of fade
 local bool_TransportAutoStop = true -- stops transport automatically after auditioning
 
--- variables
+---------------
+-- variables --
+---------------
+
 local r = reaper
 
 local itemGUID_temp
@@ -13,14 +42,16 @@ local modulePath = ({r.get_action_context()})[2]:match("^.+[\\/]")
 package.path = modulePath .. "?.lua"
 local so = require("soapy-seahorse_Fades_Functions")
 
-------------------------------------------------
+----------
+-- main --
+----------
 
 function main()
 
   r.Undo_BeginBlock()
   r.PreventUIRefresh(1)
 
-  local bool_success, item1GUID, item2GUID, firstOrSecond = so.GetItemsNearMouse(fadeLenMultiplier)
+  local bool_success, item1GUID, item2GUID, firstOrSecond = so.GetItemsNearMouse(cursorBias)
 
   if bool_success then
 
@@ -41,7 +72,9 @@ function main()
 
 end
 
-------------------------------------------------
+---------------
+-- functions --
+---------------
 
 function CheckPlayState()
 
@@ -69,6 +102,8 @@ function CheckPlayState()
 
 end
 
-------------------------------------------------
+--------------------------------
+-- main execution starts here --
+--------------------------------
 
 main()
